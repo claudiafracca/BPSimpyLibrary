e99d32c0bbb8f4ee4eb6ac60047b3f3bd893afcc
claudiafracca <50914096+claudiafracca@users.noreply.github.com>
2021-05-14 17:03:07 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     version='0.1.0',
     description='BPSim Library',
     author='Angelica Bianconi, Francesca Meneghello, Claudia Fracca, Massimiliano De Leoni',
-    install_requires=['elementpath','lxml','datetime','pandas','icalendar', 'isodate']
+    install_requires=['elementpath','lxml>=4.5','datetime','pandas','icalendar', 'isodate']
 )
