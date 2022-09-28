1e4a4ffa166152f62372a020c1b573ffb17a64f6
Francesca Meneghello <39118896+TraSci@users.noreply.github.com>
2022-09-28 15:16:31 +0200

    Add GNU GPL Licenses

--- BPSimpy/PropertyParameter.py
+++ BPSimpy/PropertyParameter.py
@@ -1,3 +1,21 @@
+'''
+Copyright 2021, ESTECO s.p.a 
+
+This file is part of BPSimpyLibrary.
+
+BPSimpyLibrary is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation version 3 of the License.
+
+BPSimpyLibrary is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with BPSimpyLibrary. If not, see <https://www.gnu.org/licenses/>.
+
+This project was developed by Angelica Bianconi, Claudia Fracca, Francesca Meneghello with the supervision of  
+Fabio Asnicar, Massimiliano de Leoni, Alessandro Turco, as part of the collaboration between ESTECO s.p.a and the University of Padua
+
+'''
+
 from BPSimpy.Parameter import Parameter
 from BPSimpy import utility
 import xml.etree.ElementTree as ET
